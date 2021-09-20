5f627f0a45f7fb0882bf528fc1482e1693d90c51
gabrielcoronin <gabrielcoronin@gmail.com>
2021-09-20 09:58:32 -0300

    feat: add controllers

---
+++ src/RushToWin/RushToWin.API/Controllers/TransactionsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RushToWin.API.Domain.Entities;
+using RushToWin.API.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RushToWin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+
+        private readonly ITransactionService _transactionService;
+
+        public TransactionsController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpPost]
+        [Route("recharge")]
+        public async Task<ActionResult<Transaction>> Recharge(double value, Guid walletId)
+        {
+            var result = await _transactionService.Recharge(value, walletId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("bus")]
+        public async Task<ActionResult<Transaction>> Bus(Guid walletId)
+        {
+            var result = await _transactionService.Bus(walletId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("subway")]
+        public async Task<ActionResult<Transaction>> Subway(Guid walletId)
+        {
+            var result = await _transactionService.Subway(walletId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("train")]
+        public async Task<ActionResult<Transaction>> Train(Guid walletId)
+        {
+            var result = await _transactionService.Train(walletId);
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> List(Guid walletId)
+        {
+            var result = await _transactionService.List(walletId);
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ src/RushToWin/RushToWin.API/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RushToWin.API.Domain.Entities;
+using RushToWin.API.Domain.Interfaces.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace RushToWin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(Guid id)
+        {
+            var result = await _userService.Get(id);
+            return Ok(result);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string fullName, string cpf, string email, string password)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid(),
+                FullName = fullName,
+                CPF = cpf,
+                Email = email,
+                Password = password,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+
+            var result = await _userService.Update(user);
+
+            return Ok(result);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(string fullName, string cpf, string email, string password)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid(),
+                FullName = fullName,
+                CPF = cpf,
+                Email = email,
+                Password = password,
+                CreatedAt = new DateTime(),
+                UpdatedAt = new DateTime()
+            };
+
+            var result = await _userService.Insert(user);
+
+            return Ok();
+        }      
+
+    }
+}
