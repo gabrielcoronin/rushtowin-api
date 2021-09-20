a6d84e957fed6edb174ac80dddaaaacaf9496c9a
gabrielcoronin <gabrielcoronin@gmail.com>
2021-09-20 09:57:45 -0300

    feat: add dependency injection config

---
+++ src/RushToWin/RushToWin.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using RushToWin.API.Data.Context;
+using RushToWin.API.Data.Repositories;
+using RushToWin.API.Domain.Interfaces.Repositories;
+using RushToWin.API.Domain.Interfaces.Services;
+using RushToWin.API.Domain.Services;
+
+namespace RushToWin.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MyDbContext>();
+
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IWalletRepository, WalletRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped<IWalletService, WalletService>();
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+
+            return services;
+        }
+    }
+}
--- src/RushToWin/RushToWin.API/Startup.cs
+++ src/RushToWin/RushToWin.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using RushToWin.API.Configuration;
 using RushToWin.API.Data.Context;
 using System;
 
@@ -28,6 +29,7 @@ namespace RushToWin.API
                 options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 11)));
             });
 
+            services.ResolveDependencies();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,6 +45,14 @@ namespace RushToWin.API
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RushToWin.API v1"));
+            }
+
+            using (var serviceScope = app.ApplicationServices
+                    .GetRequiredService<IServiceScopeFactory>()
+                    .CreateScope())
+            {
+                using var context = serviceScope.ServiceProvider.GetService<MyDbContext>();
+                context.Database.Migrate();
             }
 
             app.UseHttpsRedirection();
